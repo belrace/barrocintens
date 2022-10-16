6d270c7f3579099b227fd61e7ffa91b395bba8ec
githubBram01 <93133849+githubBram01@users.noreply.github.com>
2022-10-16 20:57:28 +0200

    products add categories add

--- app/Http/Controllers/ProductCategoriesController.php
+++ app/Http/Controllers/ProductCategoriesController.php
@@ -13,9 +13,16 @@ class ProductCategoriesController extends Controller
         $catgory = product_categories::all();
         return view('dashboards.categories.show');
     }
-    public function store(){
+    public function store(Request $request){
         $newcategory = new product_categories();
-        $newcategory->name = Input::get();
+        $newcategory->name = $request->get('catname');
+        $checkonlyemployee = $request->get('employeeonly');
+        if ($checkonlyemployee == null){
+            $newcategory->is_employee_only = 0;
+        }
+        else{
+            $newcategory->is_employee_only = 1;
+        }
         $newcategory->save();
         return view('dashboards.categories.show');
     }
