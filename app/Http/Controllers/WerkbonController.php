4f42409cbcb90448ba3ea3c6c139d0bea4ea7b8c
DarkMrAnime <gyllian121@gmail.com>
2022-10-20 10:37:06 +0200

    Icons, sidebar maintenannce and werkbon

--- app/Http/Controllers/WerkbonController.php
+++ app/Http/Controllers/WerkbonController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class WerkbonController extends Controller
 {
-    //
+    public function getWerkbon()
+    {
+        return view('dashboards.maintenance.werkbon');
+    }
+    public function store()
+    {
+    }
 }
