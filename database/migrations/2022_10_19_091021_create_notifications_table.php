fc341ab77a934c713dfd71586039ce07e3bde9a4
ruben <rubenvanbeveren03@gmail.com>
2022-11-02 11:03:00 +0100

    gino

--- database/migrations/2022_10_19_091021_create_notifications_table.php
+++ database/migrations/2022_10_19_091021_create_notifications_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longtext('description');
+            $table->string('werknemer');
             $table->timestamps();
         });
     }
