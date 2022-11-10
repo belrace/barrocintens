bcd34f69289fdf530efa7cc74c374f601e42f0cb
DarkMrAnime <gyllian121@gmail.com>
2022-11-10 10:35:56 +0100

    a

--- resources/views/dashboards/maintenance/werkbon.blade.php
+++ resources/views/dashboards/maintenance/werkbon.blade.php
@@ -400,11 +400,11 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                                         </svg>
                                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                                        <form action="{{ route('dashboard.werkuren.destroy', $hours->id) }}" method="POST>
+                                        <form action="{{ route('dashboard.werkuren.destroy', $hours->id) }}" method="POST">
                                             @csrf
                                             @method('delete')
                                             <button data-modal-toggle=" popup-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                                            Yes, I'm sure
+                                                Yes, I'm sure
                                             </button>
                                         </form>
                                         <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
