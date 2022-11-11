cc6d2b2457bb9579a87db3f9931dee680fd47678
DarkMrAnime <gyllian121@gmail.com>
2022-11-11 15:26:42 +0100

    Seeders gevuld

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -104,5 +104,15 @@ class DatabaseSeeder extends Seeder
             ['id' => 2, 'name' => 'Bonen', 'is_employee_only' => 0],
         ];
         DB::table('product_categories')->insert($categories);
+
+        $products = [
+            ['id' => 1, 'name' => 'Bit-Deluxe', 'description' => 'The better version of the Bit collection!', 'image_path' => 'machinebitdeluxe2.png', 'brand' => 'Bit', 'price' => 499, 'product_category_id' => 1],
+            ['id' => 2, 'name' => 'Bit-Light', 'description' => 'The lesser version of the Bit collection!', 'image_path' => 'machinebitlight1.png', 'brand' => 'Bit', 'price' => 799, 'product_category_id' => 1],
+            ['id' => 3, 'name' => 'Espresso Beneficio', 'description' => 'Een toegankelijke en zachte koffie. Hij is afkomstig van de Finca El Limoncillo, een weelderige plantage aan de rand van het regenwoud in de Matagalpa regio in Nicaragua.', 'image_path' => 'bonenzak1.png', 'brand' => 'Espresso Beneficio', 'price' => 21.60, 'product_category_id' => '2'],
+            ['id' => 4, 'name' => 'Yellow Bourbon Brasi', 'description' => 'Koffie van de oorspronkelijke koffiestruik (de Bourbon) met gele koffiebessen. Deze zeldzame koffie heeft de afgelopen 20 jaar steeds meer erkenning gekregen en vele prijzen gewonnen.', 'image_path' => 'bonenzak2.png', 'brand' => 'Yellow Bourbon Brasil', 'price' => 23.20, 'product_category_id' => '2'],
+            ['id' => 5, 'name' => 'Espresso Roma', 'description' => 'Een Italiaanse espresso met een krachtig karakter en een aromatische afdronk.', 'image_path' => 'bonenzak3.png', 'brand' => 'Espresso Roma', 'price' => 20.80, 'product_category_id' => '2'],
+            ['id' => 6, 'name' => 'Red Honey Honduras', 'description' => 'De koffie is geproduceerd volgens de honey-methode. Hierbij wordt de koffieboon in haar vruchtvlees gedroogd, waardoor de zoete fruitsmaak diep in de boon trekt. Dit levert een éxtra zoete koffie op.', 'image_path' => 'bonenzak4.png', 'brand' => 'Red Honey Honduras', 'price' => 27.80, 'product_category_id' => '2'],
+        ];
+        DB::table('products')->insert($products);
     }
 }
